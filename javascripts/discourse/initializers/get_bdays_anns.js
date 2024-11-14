2616de3c60a300b9d6428564ee42eb635cfa267b
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2024-11-14 17:19:08 +0800

    Update get_bdays_anns.js

--- javascripts/discourse/initializers/get_bdays_anns.js
+++ javascripts/discourse/initializers/get_bdays_anns.js
@@ -61,12 +61,7 @@ export default apiInitializer("1.14.0", (api) => {
                 }
             }
 
-            <template>
-                <p>Hi there</p>
-                              
-                              
-            </template>
-            /*
+
             <template>
                 <div class='bd-anns-banner'>
                     <div class='birthdays'>
@@ -89,7 +84,7 @@ export default apiInitializer("1.14.0", (api) => {
                     </div>
                 </div>
             </template>
-            */
+
         }
     );
 });
