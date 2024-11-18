309bfd4c8b64da3e3110d15ed2f63b0161e71993
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2024-11-18 17:59:25 +0800

    Update and rename bdaysAnnsBanner.js to bdaysAnnsBanner.gjs

--- javascripts/discourse/initializers/bdaysAnnsBanner.js
+++
@@ -1,6 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-import bdaysAnnsBanner from "../components/get_bdays_anns";
-
-export default apiInitializer("1.14.0", (api) => {
-  api.renderInOutlet('below-site-header', bdaysAnnsBanner);
-});
