a956128b59fcaf7d89d60497441a0b8617b37dc7
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2024-11-18 17:51:53 +0800

    Update bdaysAnnsBanner.js

--- javascripts/discourse/initializers/bdaysAnnsBanner.js
+++ javascripts/discourse/initializers/bdaysAnnsBanner.js
@@ -1,5 +1,5 @@
 import { apiInitializer } from "discourse/lib/api";
-import bdaysAnnsBanner from "../components/get_bdays_anns";
+import bdaysAnnsBanner from "../components/get_bdays_anns.gjs";
 
 export default apiInitializer("1.14.0", (api) => {
   api.renderInOutlet('below-site-header', bdaysAnnsBanner);
